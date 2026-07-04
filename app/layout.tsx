eca9ae88724d7698c261d15713594b1c262d7f5e
HAMVOICE <hamvoice@HAMVOICE-Macmini.local>
2026-07-04 21:44:43 +0900

    fix safe area inset

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,12 @@ export const metadata: Metadata = {
   },
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  viewportFit: "cover",
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -43,7 +49,7 @@ export default function RootLayout({
       lang="ko"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" style={{ paddingTop: 'env(safe-area-inset-top)', paddingBottom: 'env(safe-area-inset-bottom)' }}>{children}</body>
     </html>
   );
 }
