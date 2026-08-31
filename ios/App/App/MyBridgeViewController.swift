e8aea2dc0202ee0179f1f1c2b17e43074734162c
HAMVOICE <hamvoice@HAMVOICEui-MacBookAir.local>
2026-08-31 17:30:33 +0900

    feat: iOS 가장자리 스와이프로 뒤로가기/앞으로가기 제스처 활성화

--- ios/App/App/MyBridgeViewController.swift
+++ ios/App/App/MyBridgeViewController.swift
@@ -6,5 +6,7 @@ class MyBridgeViewController: CAPBridgeViewController {
     override open func capacitorDidLoad() {
         super.capacitorDidLoad()
         bridge?.registerPluginInstance(WidgetDataPlugin())
+        // 아이폰 가장자리 스와이프로 뒤로가기/앞으로가기 제스처 활성화
+        webView?.allowsBackForwardNavigationGestures = true
     }
 }
